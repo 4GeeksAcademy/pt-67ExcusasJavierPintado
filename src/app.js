1a9e9ece8e21744d947c2d7995cab6f86f6acb5d
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-05-14 12:46:15 +0000

    forst commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+function generarEscusa() {
+  let randomwho = who[Math.floor(who.length * Math.random())];
+  let randomaction = action[Math.floor(who.length * Math.random())];
+  let randomwhat = what[Math.floor(who.length * Math.random())];
+  let randomwhen = when[Math.floor(who.length * Math.random())];
+  const excusaAleatoria = `$ {randomwho} $ {randomaction} $ {randomwhat} $ {randomwhen}. `;
+  return excusaAleatoria;
+}
