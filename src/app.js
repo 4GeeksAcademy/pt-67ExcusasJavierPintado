188f7e478c91c88fca783a787fd46f3bb2ab2c7f
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-05-14 15:29:58 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,21 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "The mailman", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "my phone", "the car"];
-let when = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying"
-];
-function generarEscusa() {
-  let randomwho = who[Math.floor(who.length * Math.random())];
-  let randomaction = action[Math.floor(who.length * Math.random())];
-  let randomwhat = what[Math.floor(who.length * Math.random())];
-  let randomwhen = when[Math.floor(who.length * Math.random())];
-  const excusaAleatoria = `$ {randomwho} $ {randomaction} $ {randomwhat} $ {randomwhen}. `;
-  return excusaAleatoria;
-}
+window.onload = function() {
+  //write your code here
+  // document.getElementById("excuse").addEventListener("click", () => {
+  //   document.getElementById("excuse").innerHTML = excuse();
+  // });
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function yo() {
+    let randomwho = who[Math.floor(who.length * Math.random())];
+    let randomaction = action[Math.floor(who.length * Math.random())];
+    let randomwhat = what[Math.floor(who.length * Math.random())];
+    let randomwhen = when[Math.floor(who.length * Math.random())];
+    const excusaAleatoria = `${randomwho} ${randomaction} ${randomwhat} ${randomwhen}.`;
+    return excusaAleatoria;
+  }
+  let miExcuse = yo();
+  let mifrase = document.getElementById("excuse");
+  mifrase.innerHTML = miExcuse;
+};
